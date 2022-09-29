9fefb049fcbd7a6c25d52917c9376d4b202af301
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:37:45 -0500

    Initial commit

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa numero!");
+int n;
+n=Console.Read();
+bool esprimo = true;
+if (n <= 3)
+{
+    Console.WriteLine("Es primo");
+}
+else
+{
+ for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            esprimo = false;
+            break;
+        }
+    }
+}
+if (esprimo)
+{
+    Console.WriteLine("Es Primo");
+}
+else
+{
+    Console.WriteLine("No es Primo");
+}
